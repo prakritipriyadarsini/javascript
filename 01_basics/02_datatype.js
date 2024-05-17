3f6fbee3464674858d1d7e61eccdce32c718c4c2
kriti <160092243+prakritipriyadarsini@users.noreply.github.com>
2024-05-17 14:45:57 +0000

    datatypes in js

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,36 @@
+"use strict";  // treat all js code as newer version
+
+//alert ( 5 + 9 ) 
+//we are using nodejs ,not browser
+
+console.log(5
+    +9);                  //14
+
+console.log("kriti");    //kriti
+
+//code readability should be high
+// TC39.es original documentation 
+
+
+
+let name = "kriti"
+let age = 20
+let isloggedIn = false
+let state;
+
+// number => 2 to power 53 (range in memory address)
+//bigInt 
+//String => ""
+//boolean => true or false
+//null=> standalone value
+//undefined => value not assigned
+//symbol => define unique
+
+//object
+
+console.log(typeof age); // Number
+console.log(typeof "kriti"); //string
+console.log(typeof "name"); //string
+console.log(typeof isloggedIn); //boolean
+console.log(typeof state);  //undefined 
+console.log(typeof null);   //object
