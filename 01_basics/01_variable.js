8e9d3e1c190c9ffc28d8eb9148d2bae28cda2b17
kriti <160092243+prakritipriyadarsini@users.noreply.github.com>
2024-05-17 14:10:56 +0000

    variables in js

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,21 @@
+const accountId = 1232343
+let accountEmail = "kriti@gmail.com"  //prakritipriyadarsiniswain7653@gmail.com 
+var accountPassword ="3422343"  //545676543
+accountCity = "Bhubaneswar" //cuttack
+let accountState;  //undefined
+
+// account = 2
+// not alloweded
+console.log(accountId)
+
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+accountEmail = "prakritipriyadarsiniswain7653@gmail.com "
+accountPassword = "545676543"
+accountCity = "cuttack"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
