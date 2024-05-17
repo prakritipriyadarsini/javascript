d12d517ee7b5b5cbf27c68b3f3b139070b986bef
kriti <160092243+prakritipriyadarsini@users.noreply.github.com>
2024-05-17 16:42:05 +0000

    operations in js

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -31,3 +31,41 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);            //33
 console.log(typeof stringNumber);    //string
 
+
+
+//----------------------------------operation-----------------------------
+
+let value = 3
+let negValue = -value 
+console.log(negValue);  //  -3
+
+console.log( 2 + 2 );     // 4
+console.log( 2 - 2 );     // 0
+console.log( 2 * 2 );     // 4
+console.log( 2 ** 2 );    // 4
+console.log( 2 / 3 );     // 0.666666666666
+console.log( 2 % 3 );     // 2
+
+
+let str1 = " hello"
+let str2 = " kriti "
+
+let str3 = str1 + str2
+console.log(str3);       // hello kriti
+
+//-------------------------------------------------------------
+
+console.log( "1" + 2 );   // 12
+console.log( 1 + "2" );  //12
+
+//--------------------------------------------------------
+console.log(true +true);
+
+console.log(2 + true );
+
+//-------------------------------------------------------------
+
+
+
+
+
