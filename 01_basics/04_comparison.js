c4013c87bc524d1017d48c7e37eccecef9062d6f
kriti <160092243+prakritipriyadarsini@users.noreply.github.com>
2024-05-18 05:13:01 +0000

    comparison of datatype in js

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,28 @@
+console.log( 2 > 1 );    // T
+console.log( 2 >= 1 );   // T
+console.log( 2 == 2 );    // T
+console.log( 2 != 1 );    // T
+console.log(2 ==1);      //F
+
+
+
+
+console.log( "2" > 1);    // T
+console.log( "02" > 1);   // T
+ 
+
+console.log( null > 0 );    // F
+console.log( null == 0 );   // F
+console.log( null >= 0 );   // T
+console.log( null < 0 );   // F
+
+console.log( undefined == 0);    // F
+console.log(undefined > 0  );    // F
+console.log( undefined < 0);    // F
+
+// === 
+
+console.log( "2" === 2); //false
+
+
+
