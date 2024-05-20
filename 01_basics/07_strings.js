20dbe6e0794ece6f03b686d78e7b6d4f6dfd5efa
Prakriti Priyadarsini Swain <160092243+prakritipriyadarsini@users.noreply.github.com>
2024-05-20 06:16:15 +0000

    Strings in js

---
+++ 01_basics/07_strings.js
@@ -0,0 +1,10 @@
+const name = "kriti"
+const repoCount = 30
+
+console.log( name + repoCount +"value"); // kriti30value
+         //this method is not beter for us ( its oldmethod).
+
+     //    `` - this is called backtick
+     // use to concatenate (...................... ${..}............${}............)
+     
+
