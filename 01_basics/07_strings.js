0f5aab6080632d5e5706032fa65f4b2c72c4e902
Prakriti Priyadarsini Swain <160092243+prakritipriyadarsini@users.noreply.github.com>
2024-05-21 06:33:51 +0000

    method of string

--- 01_basics/07_strings.js
+++ 01_basics/07_strings.js
@@ -6,5 +6,50 @@ console.log( name + repoCount +"value"); // kriti30value
 
      //    `` - this is called backtick
      // use to concatenate (...................... ${..}............${}............)
-     
+
+     console.log( ` Hello my name is ${name} and my repo count is ${ repoCount}`);
+     // Hello my name is kriti and my repo count is 30
+
+
+const gameName = new String ( 'prakriti priyadarsini swain')
+
+console.log( gameName[0]);             // p
+console.log( gameName.__proto__);      //  {}
+
+
+console.log( gameName.length);         // 27   ( String length)
+//The length property returns the length of a string:
+
+/*
+A string is converted to upper case with toUpperCase():
+
+A string is converted to lower case with toLowerCase():
+
+*/
+
+console.log( gameName.toUpperCase());   // PRAKRITI PRIYADARSINI SWAIN ( String toUpperCase() )
+console.log( gameName.toLowerCase());   // prakriti priyadarsini swain  ( String toLowerCase())
+
+/* text.split(",")    // Split on commas
+text.split(" ")    // Split on spaces
+text.split("|")    // Split on pipe */
+
+console.log( gameName.split(" "));         // [ 'prakriti', 'priyadarsini','swain']
+
+// The trim() method removes whitespace from both sides of a string:
+
+
+
+
+
+
+//-------------------in js using css use '%c' -----------------------
+
+// console.log("%c hello world " , " color: blue,font-size:40px");
+// console.log("%cHello, World", "color: blue; font-size: 40px");
+
+// ------------------------------------------------------------
+
+
+
 
